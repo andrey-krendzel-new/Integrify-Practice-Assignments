59aecaad09f2f7cc78f4a3192f0dc81ad792bdc6
andrey-krendzel-new <andrey.pandaoy@gmail.com>
2024-08-02 14:30:54 +0300

    Integrify React and Node projects

---
+++ integrify_react_project/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Button>
+      </Button>
+      <Modal>
+      </Modal>
+      <TodoList></TodoList>
+    </div>
+  );
+}
+
+export default App;
